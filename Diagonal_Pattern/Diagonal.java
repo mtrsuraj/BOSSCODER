7c927c7df12593fdf2ff2fc9d925e11d36e2ca6c
mtrsuraj <surajprintf33@gmail.com>
2023-06-16 18:53:20 +0530

    code upgrade

--- Diagonal_Pattern/Diagonal.java
+++ Diagonal_Pattern/Diagonal.java
@@ -6,14 +6,14 @@ public class Diagonal {
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(System.in)){
             int n= scanner.nextInt();
-        int sp=1;
         for (int i=1; i<=n; i++) {
-
-            System.out.print("*\n");
-            for(int j=1; j<=sp; j++) {
-            System.out.print("\t");
+            for(int j=1; j<=n; j++) {
+                if(i==j){
+                    System.out.print("*\n");
+                }else System.out.print("\t");
             }
-            sp++;
+            System.out.println();
+            
         }
     }
     }
