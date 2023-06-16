51d6a897cd5cf6fb025698361fb852632b7de86f
mtrsuraj <surajprintf33@gmail.com>
2023-06-16 17:00:06 +0530

    Diamond Pattern

---
+++ Diamond_pattern/Daimond.java
@@ -0,0 +1,29 @@
+package Diamond_pattern;
+
+import java.util.Scanner;
+
+public class Daimond{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter No: ");
+        int n = scanner.nextInt();
+        int sp = n/2;
+        int st = 1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sp; j++){
+                System.out.print("\t");
+        }
+        for(int j=1; j<=st; j++){
+            System.out.print("*\t");
+        }
+        if(i<=n/2){
+            sp--;
+            st+=2;
+        }else {
+            sp++;
+            st-=2;
+        }
+        System.out.println();
+    }
+    }
+}
